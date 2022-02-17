13ed388d763e4801b4e470bd11543e2526ee786a
Pablo Mateos García <34125654+pablomatgar@users.noreply.github.com>
2022-02-17 17:22:11 +0100

    Update Capital.java

--- src/bluecow/Capital.java
+++ src/bluecow/Capital.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bluecow;
 
-/**
- *
- * @author S19001531
- */
 class Capital {
     private String name;
     private String countryName;
